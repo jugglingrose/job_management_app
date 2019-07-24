ad95b11bbf57abf9116b31712247afbf75b2d368
Corean Gonzales <coreangonzales@gmail.com>
2019-07-24 17:03:23 -0500

    Added dummy data to state in app

--- src/App.js
+++ src/App.js
@@ -6,18 +6,55 @@ import DisplaySingleList from './components/DisplaySingleList.js';
 import NavBar from './components/NavBar';
 
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/overview" component={DisplayOverview} />
-          <Route exact path="/DisplaySingleList" component={DisplaySingleList} />
-        </Switch>
-      </BrowserRouter>,  
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: 
+        [ 
+          { 
+            "name": "companyX",
+            "status": "interested",
+            "about": "this is a summary about Company X",
+            "why": "this is a little info about why I want to work for Company X",
+            "notes": "my notes will go here",
+          },
+          { 
+            "name": "companyY",
+            "status": "applied",
+            "about": "this is a litte info about why I want to work for CompanyY",
+            "notes": "my notes will go here",
+          },
+          {
+            "name": "companyZ",
+            "status": "interviewing",
+            "about": "this is a little info about why I wanto to work for Company Z",
+            "notes": "my notes will go here",
+          },
+          {
+            "name": "companyNO",
+            "status": "denied",
+            "about": "this is a little info about why I wanto to work for Company NO",
+            "notes": "my notes will go here",
+          }
+        ]
+      };
+    }
+  
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/overview" component={DisplayOverview} />
+            <Route exact path="/DisplaySingleList" component={DisplaySingleList} />
+          </Switch>
+        </BrowserRouter>,  
+      </div>
+    );
+  }
+  
 }
 
 export default App;
